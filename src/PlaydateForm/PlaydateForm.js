c647e64278897d774914426672b892757c9895f9
Nolan Caine <nolancaine@gmail.com>
2021-10-30 17:38:12 -0600

    Create PlaydateForm component

---
+++ src/PlaydateForm/PlaydateForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Route, Switch, Link, Redirect } from 'react-router-dom'
+
+class PlaydateForm extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <h1>Form</h1>
+    )
+  }
+}
+
+export default PlaydateForm
