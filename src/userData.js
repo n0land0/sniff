2818790c70b7c9835e5f925a1ada97ff189ec792
Nolan Caine <nolancaine@gmail.com>
2021-10-29 23:02:30 -0600

    Add dummy data file

---
+++ src/userData.js
@@ -0,0 +1,34 @@
+const userData = [
+  {
+    ownerName: 'Nolan',
+    dogName:  'Biscuit',
+    profilePic: 'https://i.imgur.com/i4UwYMu.jpeg',
+    dogBreed: 'Sheppard',
+  },
+  {
+    ownerName: 'Joshua',
+    dogName: 'Fido',
+    profilePic: 'https://i.imgur.com/LRoLTlK.jpeg',
+    dogBreed: 'Labrador',
+  },
+  {
+    ownerName: 'Paul',
+    dogName: 'Twinky',
+    profilePic: 'https://i.imgur.com/ZMPYHsk.jpeg',
+    dogBreed: 'Pit Bull',
+  },
+  {
+    ownerName: 'Robbie',
+    dogName: 'Chunk',
+    profilePic: 'https://i.imgur.com/tIqs5vu.jpeg',
+    dogBreed: 'Wiener',
+  },
+  {
+    ownerName: 'Kayla',
+    dogName: 'Kevin',
+    profilePic: 'https://i.imgur.com/ZKGFJbC.jpeg',
+    dogBreed: 'Corgi',
+  }
+]
+
+export default userData
