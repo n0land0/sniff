0afa42be27b0ed44b94f3009e84f3c6c7eae4af7
Joshua Benefield <benefield.joshua@gmail.com>
2021-10-30 01:56:57 -0600

    Remove find user method, and change profile props to just id

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch, Link, Redirect } from 'react-router-dom'
 import './App.css'
+import Profile from '../Profile/Profile'
 
 import Gallery from '../Gallery/Gallery'
 
@@ -16,9 +17,11 @@ class App extends React.Component {
     this.setState({ users: userData })
   }
 
-  findUser = (userId) => {
-    return users.find(user => user.id === userId);
-  }
+  // findUser = (users, userId) => {
+  //   const selectedProfile = this.findUser(users, match.params.id,)
+  //
+  //   return users.find(user => user.id === userId);
+  // }
 
   render() {
     const { users } = this.state
@@ -29,10 +32,9 @@ class App extends React.Component {
         <Route path='/findfriends' render={ () =>
           <Gallery users={users}/>
         } />
-        <Route path='/profile/:userId' render={ ({ match }) => {
-          const selectedProfile = this.findUser(match.params.id)
-          return <Profile {...selectedProfile}/>
-        } }/>
+        <Route path='/profile/:userId' render={ ({ match }) =>
+          <Profile id={match.params.userId}/>
+        } />
       </main>
     )
   }
