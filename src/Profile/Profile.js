01b78350e93908b7d0bf04e9345bdeb1f8b721ef
Nolan Caine <nolancaine@gmail.com>
2021-10-30 00:48:42 -0600

    Create Profile directory

---
+++ src/Profile/Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Route, Switch, Link, Redirect } from 'react-router-dom'
+
+import userData from '../userData'
+
+class Profile extends Component {
+  state = {
+
+  }
+
+  componentDidMount = () => {
+    // fetch
+
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Profile
