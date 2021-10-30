090e731e5a4eee27027353ee0d702ae012aaadad
Joshua Benefield <benefield.joshua@gmail.com>
2021-10-29 21:53:21 -0600

    Build App and Index structure

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from './react'
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <main>
+
+      </main>
+    )
+  }
+}
+
+export default App
