bf6474498fbce7c68d9c6b95f39de43632409910
Nolan Caine <nolancaine@gmail.com>
2021-10-31 00:33:28 -0600

    Replace Header component with Nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Route, Switch, Link, NavLink, Redirect } from 'react-router-dom'
+import './Nav.css'
+
+const Nav = () => {
+
+  return (
+    <nav>
+      <NavLink exact to='/dashboard' activeClassName='active'>
+        Home
+      </NavLink>
+      <NavLink exact to='/findfriends' activeClassName='active'>
+        Find Friends
+      </NavLink>
+    </nav>
+  )
+}
+
+export default Nav
