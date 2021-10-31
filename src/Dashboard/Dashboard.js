26526d2cee61f8eb7e1964c39011103446c0b596
Nolan Caine <nolancaine@gmail.com>
2021-10-31 01:33:02 -0600

    Display playdates

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Switch, Link, Redirect } from 'react-router-dom'
+
+import Playdate from '../Playdate/Playdate'
 import './Dashboard.css'
 
+const Dashboard = ({ appointments, users }) => {
+  const playdates = appointments.map(appt => {
+    appt.userToMeet = appt.usersInvolved.filter(userId =>
+      users.some(user => user.id === userId)
+    ).map(userId =>
+      users.find(user => user.id === userId)
+    )[0]
+    return <Playdate key={appt.id} {...appt}/>
+  })
 
+  return (
+    <>
+      <section>
+      </section>
+      <section>
+        {playdates}
+      </section>
+    </>
+  )
+}
 
 export default Dashboard
