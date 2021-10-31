9069917ab5c1c7cb20ffe5ff160eee880916f424
Nolan Caine <nolancaine@gmail.com>
2021-10-30 18:03:52 -0600

    Build form inputs

--- src/PlaydateForm/PlaydateForm.js
+++ src/PlaydateForm/PlaydateForm.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react'
 import { Route, Switch, Link, Redirect } from 'react-router-dom'
 
+import parksData from '../parksData'
+
 class PlaydateForm extends Component {
   state = {
+    date: '',
+    location: '',
+    parks: []
+  }
 
+  componentDidMount = () => {
+    // fetch
+    this.setState({ parks: parksData })
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
   }
 
   render() {
+    const { date, location, parks } = this.state
+    const parkOptions = parks.map(park => <option value={park} >{park}</option>)
+
     return (
-      <h1>Form</h1>
+      <form>
+        <input
+          type='date'
+          name='date'
+          value={date}
+          required
+          onChange={this.handleChange}
+        />
+        <select
+          name='location'
+          value={location}
+          onChange={this.handleChange}
+        >
+          {parkOptions}
+        </select>
+      </form>
     )
   }
 }
