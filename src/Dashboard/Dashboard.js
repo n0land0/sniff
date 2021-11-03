1eda1375d9479dc0048e923ad060d35911480df3
Nolan Caine <nolancaine@gmail.com>
2021-11-03 00:54:19 -0600

    Render sorted playdates

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -5,14 +5,16 @@ import Playdate from '../Playdate/Playdate'
 import './Dashboard.css'
 
 const Dashboard = ({ appointments, users }) => {
-  // const playdates = appointments.map(appt => {
+  const playdates = appointments.sort((apptA, apptB) =>
+    new Date(apptB.date) - new Date(apptA.date)
+  ).map(appt =>
   //   appt.userToMeet = appt.usersInvolved.filter(userId =>
   //     users.some(user => user.id === userId)
   //   ).map(userId =>
   //     users.find(user => user.id === userId)
   //   )[0]
-  //   return <Playdate key={appt.id} {...appt}/>
-  // })
+    <Playdate key={appt.id} {...appt}/>
+  )
 
   // fetch user when App mounts, pass in appts, iterate and return Playdate components
 
@@ -21,6 +23,7 @@ const Dashboard = ({ appointments, users }) => {
       <section>
       </section>
       <section>
+      {playdates}
       </section>
     </>
   )
