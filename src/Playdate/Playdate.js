e1d7a470cff3abbea32d089d6f74bfc77a646201
Nolan Caine <nolancaine@gmail.com>
2021-11-03 00:55:12 -0600

    Edit props to reflect new playdate properties

--- src/Playdate/Playdate.js
+++ src/Playdate/Playdate.js
@@ -3,14 +3,15 @@ import { Route, Switch, Link, Redirect } from 'react-router-dom'
 
 import './Playdate.css'
 
-const Playdate = ({ id, date, location, userToMeet }) => {
-  const { ownerName, dogName, dogBreed } = userToMeet
+const Playdate = ({ id, date, location, playmate }) => {
+  // const { ownerName, dogName, dogBreed } = userToMeet
+  const { ownerName, dogName } = playmate
 
   return (
     <article className='playdate'>
       <p>{date}</p>
       <p>{location}</p>
-      <p>{ownerName} and {dogName} the {dogBreed}</p>
+      <p>{ownerName} and {dogName}</p>
     </article>
   )
 }
