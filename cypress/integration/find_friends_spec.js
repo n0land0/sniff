f34e79f5c0d75f0b68e715c9de4fea80a817da78
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2021-11-03 19:12:55 -0600

    Add tests

--- cypress/integration/find_friends_spec.js
+++ cypress/integration/find_friends_spec.js
@@ -2,12 +2,17 @@ describe('webpage visit', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/findfriends');
-  });
-
-  // beforeEach().visit("http://localhost3000")
+  })
 
   it('should display other dogs in my area for potential playdates', () => {
     cy.get('.gallery').children().should('be.visible')
   })
+
+  it('should be able to select an owner\'s profile to see more info about their dog', () => {
+    cy.get('.profile-card').first().click()
+    cy.get('.profile').should('be.visible')
+  })
+
+
 
 })
