3159a7c4f08ac9f49008b27f318911249e80757c
Nolan Caine <nolancaine@gmail.com>
2021-11-04 20:30:37 -0600

    Remove unnecessary comments

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -8,15 +8,8 @@ const Dashboard = ({ appointments, users }) => {
   const playdates = appointments.sort((apptA, apptB) =>
     new Date(apptB.date) - new Date(apptA.date)
   ).map(appt =>
-  //   appt.userToMeet = appt.usersInvolved.filter(userId =>
-  //     users.some(user => user.id === userId)
-  //   ).map(userId =>
-  //     users.find(user => user.id === userId)
-  //   )[0]
     <Playdate key={appt.id} {...appt}/>
   )
-
-  // fetch user when App mounts, pass in appts, iterate and return Playdate components
 
   return (
     <>
