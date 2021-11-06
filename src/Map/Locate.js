3df68d33bb73ea8e005d7cc87f398270a33d7f6c
Nolan Caine <nolancaine@gmail.com>
2021-11-06 17:47:06 -0600

    Downsize show my location button

--- src/Map/Locate.js
+++ src/Map/Locate.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import target from '../assets/target.png'
+import './Map.css'
 
 const Locate = ({ panTo, center }) => {
   return (
     <>
-    <button onClick={() => {
-      navigator.geolocation.getCurrentPosition(position => {
-        panTo({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }, 14)
-      }, () => console.log('fail'))
-    }}>
+    <button
+      className='locate__show-my-location-btn'
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(position => {
+          panTo({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }, 14)
+        }, () => console.log('fail'))
+      }}
+    >
       <img src={target} alt='show my location' />
     </button>
-    <button onClick={() => {
-      navigator.geolocation.getCurrentPosition(position => {
-        panTo({
-          lat: center.lat,
-          lng: center.lng
-        }, 11)
-      }, () => console.log('fail'))
-    }}>reset map</button>
+    <button
+      className='locate__show-my-location-btn'
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(position => {
+          panTo({
+            lat: center.lat,
+            lng: center.lng
+          }, 11)
+        }, () => console.log('fail'))
+      }}
+    >reset map</button>
     </>
   )
 }
